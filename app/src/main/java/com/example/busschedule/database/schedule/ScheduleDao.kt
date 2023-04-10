d7d69c20e4c24202a2d9fa961cf3b5909017bcc7
qjin1 <qjin1@paypal.com>
2023-04-10 14:29:49 -0500

    setup the adapter
    setup the adapter
    using the adapter in both fragment
    update the list to pass to the adapter to show in the UI in coroutines
    The adapter get a clicked function as a paramter
    the adapter have a list of types and a viewholder in <>

--- app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -9,6 +9,6 @@ interface ScheduleDao {
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
     fun getAll(): List<Schedule>
 
-    @Query("SELECT * FROM schedule WHERE :stopName ORDER BY arrival_time ASC")
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
     fun getByStopName(stopName: String): List<Schedule>
 }
